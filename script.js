90b957d83eed584c7e77c3e2ba41c561d5ed4735
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-19 09:52:48 +0530

    add js

--- script.js
+++ script.js
@@ -1,57 +1,78 @@
 
 
      f1 =()=>{
+      setTimeout(()=>{
         console.log("10")
         f2=()=>{
+         setTimeout(()=>{
             console.log("9")
             f3=()=>{
+               setTimeout(()=>{
                 console.log("8")
                 f4=()=>{
+                  setTimeout(()=>{
                     console.log("7")
                     f5=()=>{
+                     setTimeout(()=>{
                         console.log("6")
-                         f6=()=>{
+                         f6=()=>{ 
+                           setTimeout(()=>{
                             console.log("5")
-                                f7=()=>{
+                                f7=()=>{ 
+                                 setTimeout(()=>{
                                 console.log("4")
                                  f8=()=>{
+                                    setTimeout(()=>{
                                     console.log("3")
                                     f9=()=>{
+                                       setTimeout(()=>{
                                         console.log("2")
                                           f10=()=>{
+                                             setTimeout(()=>{
                                             console.log("1")
                                              f11=()=>{
+                                                setTimeout(()=>{
                                                 console.log("independent day")
+                                                 },1000)
                                             }
-                                            f11();
+                                          //   f11();
+                                          },1000)
                                         }
-                                        f10();
+                                       //  f10();
+                                       },1000)
                                     }
-                                   f9();
+                                 //   f9();
+                                 },1000)
                                 }
-                               f8();
+                              //  f8();
+                              },1000)
                             }
-                           f7(); 
+                           // f7(); 
+                        },1000)
                         }
-                       f6(); 
+                     //   f6(); 
+                     },1000)
                     }
-                    f5();
+                  //   f5();
+                  },1000)
                 }
-               f4(); 
+               // f4(); 
+            },1000)
             }
-           f3(); 
+         //   f3(); 
+         },1000)
         }
-       f2();
+      //  f2();
+      },1000)
     }
-    f1();
-    const independent1= setTimeout(f1,1000);
-    const independent2= setTimeout(f2,2000);
-    const independent3= setTimeout(f3,3000);
-    const independent4= setTimeout(f4,4000);
-    const independen5t= setTimeout(f5,5000);
-    const independent6= setTimeout(f6,6000);
-    const independent7= setTimeout(f7,7000);
-    const independent8= setTimeout(f8,8000);
-    const independent9= setTimeout(f9,9000);
-    const independent10= setTimeout(f10,10000);
-    const independent11= setTimeout(f11,11000);
+   //  f1();
+   //  const independent1= setTimeout(f1,1000);
+   //  const independent2= setTimeout(f2,2000);
+   //  const independent3= setTimeout(f3,3000);
+   //  const independent4= setTimeout(f4,4000);
+   //  const independen5t= setTimeout(f5,5000);
+   //  const independent6= setTimeout(f6,6000);
+   //  const independent7= setTimeout(f7,7000);
+   //  const independent8= setTimeout(f8,8000);
+   //  const independent9= setTimeout(f9,9000);
+   //  const independent10= setTimeout(f10,10000);
